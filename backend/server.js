832207105180b885b5c1178d10f49f51a787e39f
Sumukhms <sumukhms.25@gmail.com>
2025-10-13 22:07:18 +0530

    setup completed

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+// backend/server.js
+
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('InvestIQ API is running...');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
+
+// We will add the MongoDB connection logic here later
