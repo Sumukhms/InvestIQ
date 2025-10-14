0deb24a6d39df669ed7779b0b441a61ffc2eee15
Srikanth <srikanthking0001@gmail.com>
2025-10-14 23:43:16 +0530

    correct- working

--- backend/config/passport-setup.js
+++ backend/config/passport-setup.js
@@ -2,42 +2,48 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/User');
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/api/auth/google/callback',
-      proxy: true
-    },
-    async (accessToken, refreshToken, profile, done) => {
-      const newUser = {
-        name: profile.displayName,
-        email: profile.emails[0].value,
-      };
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
-      try {
-        let user = await User.findOne({ email: newUser.email });
+if (!GOOGLE_CLIENT_ID || !GOOGLE_CLIENT_SECRET) {
+  console.warn('Google OAuth env vars missing. Skipping Google strategy. Add GOOGLE_CLIENT_ID and GOOGLE_CLIENT_SECRET to backend/.env');
+} else {
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: GOOGLE_CLIENT_ID,
+        clientSecret: GOOGLE_CLIENT_SECRET,
+        callbackURL: '/api/auth/google/callback',
+        proxy: true
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        const newUser = {
+          name: profile.displayName,
+          email: profile.emails[0].value,
+        };
 
-        if (user) {
-          done(null, user);
-        } else {
-          user = await User.create(newUser);
-          done(null, user);
+        try {
+          let user = await User.findOne({ email: newUser.email });
+
+          if (user) {
+            done(null, user);
+          } else {
+            user = await User.create(newUser);
+            done(null, user);
+          }
+        } catch (err) {
+          console.error(err);
+          done(err, null);
         }
-      } catch (err) {
-        console.error(err);
-        done(err, null);
       }
-    }
-  )
-);
+    )
+  );
+}
 
 passport.serializeUser((user, done) => {
     done(null, user.id);
 });
 
-// **This is the corrected function**
 passport.deserializeUser(async (id, done) => {
     try {
         const user = await User.findById(id);
